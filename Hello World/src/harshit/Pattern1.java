74b5e6008145880877dc785c0cd58677db6fae69
Lenovo <Lenovo@HarshitGupta.noida.cdac.in>
2019-11-20 19:12:08 +0530

    1st pattern at office

---
+++ Hello World/src/harshit/Pattern1.java
@@ -0,0 +1,15 @@
+package harshit;
+
+public class Pattern1 {
+	
+	public static void main(String args[])
+	{
+		String ptrn="*";
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i+" "+ ptrn +" " +(i+1));
+		}
+		
+	}
+
+}
