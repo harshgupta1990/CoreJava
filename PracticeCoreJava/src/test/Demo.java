0a81785459a1c9f3e72e25ed1718a5ed06e7c7f5
harsh <harsh@DESKTOP-NGNK7QQ.mshome.net>
2024-01-12 16:34:18 +0530

    push commit

---
+++ PracticeCoreJava/src/test/Demo.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+//		List<Employee> list = new ArrayList<>();
+//		
+//		Set<Employee> employees = new HashSet<>();
+//		employees.add(new Employee(1,"Harshit"));
+//		employees.add(new Employee(1,"Harshit"));
+//		
+//		System.out.println(employees);
+		
+		
+	}
+
+}
