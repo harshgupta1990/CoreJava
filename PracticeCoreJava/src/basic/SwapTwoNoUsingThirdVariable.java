28d880354300fb452f5b8b44c48805cb1724a3c7
harsh <harsh@DESKTOP-NGNK7QQ.mshome.net>
2024-03-04 12:35:18 +0530

    commit

---
+++ PracticeCoreJava/src/basic/SwapTwoNoUsingThirdVariable.java
@@ -0,0 +1,32 @@
+package basic;
+
+import java.util.Scanner;
+
+public class SwapTwoNoUsingThirdVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=0,b=0;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter two nos to be swapped :- ");
+		System.out.print("a= ");
+		a= sc.nextInt();
+		System.out.print("\nb= ");
+		b= sc.nextInt();
+		
+		int temp=0;  // 3rd temp variable
+		
+		//now logit to swap
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("Swapped nos are :- ");
+		System.out.println("a= " + a + " b= " + b);
+		
+	}
+
+}
